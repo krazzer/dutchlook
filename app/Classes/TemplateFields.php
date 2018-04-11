ab980f26160795f682104fadf6a547d7f39e7e38
Kaz van Wel <kaz@iMac-van-Kaz.local>
2018-04-11 08:26:26 +0200

    Added CMS menu and some template settings

--- app/Classes/TemplateFields.php
+++ app/Classes/TemplateFields.php
@@ -5,6 +5,7 @@ namespace Website\Classes;
 
 use KikCMS\Classes\Frontend\Extendables\TemplateFieldsBase;
 use KikCMS\Classes\Page\Template;
+use KikCMS\Classes\WebForm\Fields\ButtonField;
 use KikCMS\Classes\WebForm\Fields\WysiwygField;
 use Phalcon\Validation\Validator\PresenceOf;
 
@@ -18,6 +19,7 @@ class TemplateFields extends TemplateFieldsBase
         return [
             (new Template('default', 'Standaard', ['content'])),
             (new Template('home', 'Home')),
+            (new Template('category', 'Categorie', ['projects'])),
         ];
     }
 
@@ -28,6 +30,7 @@ class TemplateFields extends TemplateFieldsBase
     {
         return [
             'content' => (new WysiwygField('content', 'Inhoud pagina', [new PresenceOf()]))->storePage(),
+            'projects' => (new ButtonField('Projecten', 'Projecten kunnen worden bewerkt in de projecten module', 'Ga naar projecten', '/cms/projects')),
         ];
     }
 }
--- app/Classes/WebsiteSettings.php
+++ app/Classes/WebsiteSettings.php
@@ -4,6 +4,9 @@ namespace Website\Classes;
 
 
 use KikCMS\Classes\Frontend\Extendables\WebsiteSettingsBase;
+use KikCMS\Config\MenuConfig;
+use KikCMS\Services\Cms\CmsMenuGroup;
+use KikCMS\Services\Cms\CmsMenuItem;
 use Phalcon\Mvc\Router\Group;
 
 /**
@@ -30,7 +33,11 @@ class WebsiteSettings extends WebsiteSettingsBase
      */
     public function getMenuGroups(array $menuGroups): array
     {
-        return $menuGroups;
+        $webshopMenuGroup = (new CmsMenuGroup('projects', "Projecten"))
+            ->add(new CmsMenuItem('projects', 'Projecten', 'cms/projects'))
+            ->add(new CmsMenuItem('clients', 'Klanten', 'cms/clients'));
+
+        return array_add_after_key($menuGroups, MenuConfig::MENU_GROUP_CONTENT, 'projects', $webshopMenuGroup);
     }
 
     /**
