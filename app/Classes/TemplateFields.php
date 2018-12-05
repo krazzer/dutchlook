522b3202706c577c6e268acfad669ef3b7937693
Kaz van Wel <info@kiksaus.nl>
2018-12-05 09:20:23 +0100

    Convert DataTables to KeyedDataTables (2)

--- app/Classes/TemplateFields.php
+++ app/Classes/TemplateFields.php
@@ -6,7 +6,7 @@ namespace Website\Classes;
 use KikCMS\Classes\Frontend\Extendables\TemplateFieldsBase;
 use KikCMS\Classes\Page\Template;
 use KikCMS\Classes\WebForm\Fields\ButtonField;
-use KikCMS\Classes\WebForm\Fields\DataTableField;
+use KikCMS\Classes\WebForm\Fields\KeyedDataTableField;
 use KikCMS\Classes\WebForm\Fields\TextField;
 use KikCMS\Classes\WebForm\Fields\WysiwygField;
 use Phalcon\Validation\Validator\PresenceOf;
@@ -36,7 +36,7 @@ class TemplateFields extends TemplateFieldsBase
         return [
             'content'    => (new WysiwygField('content*:value', 'Inhoud pagina', [new PresenceOf()])),
             'projects'   => (new ButtonField('Projecten', 'Projecten kunnen worden bewerkt in de projecten module', 'Ga naar projecten', '/cms/projects')),
-            'homeImages' => (new DataTableField(new HomeImages, 'Afbeeldingen')),
+            'homeImages' => (new KeyedDataTableField('homeImages', HomeImages::class, 'Afbeeldingen')),
             'video'      => (new TextField('video:value', 'Video')),
         ];
     }
